48fbae1eda75b5a18a05d1779a6da22d82b9ba92
Amachivine <favour2apenefavour2apene@gmail.com>
2022-12-12 10:53:32 -0800

    Once again

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,31 +2,27 @@
 
 /**
  * main - prints out all possible different combinations of three digits.
- *
- *
  * Return: Always 0 (Success)
- *
  */
 
 int main(void)
 {
-	int ones = '0';
-	int tens = '0';
-	int hundreds = '0';
+	int ones;
+	int tens;
+	int hundreds;
 
-	for (hundreds = '0'; hundreds <= '9'; hundreds++)
+	for (hundreds = 0; hundreds <= 9; hundreds++)
 	{
-		for (tens = '0'; tens <= '9'; tens++)
+		for (tens = 0; tens <= 9; tens++)
 		{
-			for (ones = '0'; ones <= '9'; ones++)
+			for (ones = 0; ones <= 9; ones++)
 			{
-				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))/*eliminates repitition*/
+				if (hundreds > tens && tens > ones)
 				{
-					putchar(hundreds);
-					putchar(tens);
-					putchar(ones);
-					
-					if (!(ones == '9' && hundreds == '7' && tens == '8'))/*adds comas and spaces*/
+					putchar(ones + '0');
+					putchar(tens + '0');
+					putchar(hundreds + '0');
+					if (ones != 7 || tens != 8 || hundreds != 9)
 					{
 						putchar(',');
 						putchar(' ');
@@ -35,7 +31,6 @@ int main(void)
 			}
 		}
 	}
-
 	putchar('\n');
 	return (0);
 }
