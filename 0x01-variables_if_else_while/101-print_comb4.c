11870e8f6d2e2f680290b5d9ff9b00c28453127a
Amachivine <favour2apenefavour2apene@gmail.com>
2022-12-12 11:26:48 -0800

    Correction again

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
-
 /**
- * main - prints out all possible different combinations of three digits.
- * Return: Always 0 (Success)
+ * main - entry point
+ * Return: Always return 0
  */
-
 int main(void)
 {
-	int ones;
-	int tens;
-	int hundreds;
+	int i;
+	int j;
+	int k;
 
-	for (hundreds = 0; hundreds <= 9; hundreds++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (tens = 0; tens <= 9; tens++)
+		for (j = 0; j <= 9; j++)
 		{
-			for (ones = 0; ones <= 9; ones++)
+			for (k = 0; k <= 9; k++)
 			{
-				if (hundreds > tens && tens > ones)
+				if (k > j && j > i)
 				{
-					putchar(ones + '0');
-					putchar(tens + '0');
-					putchar(hundreds + '0');
-					if (ones != 7 || tens != 8 || hundreds != 9)
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i != 7 || j != 8 || k != 9)
 					{
 						putchar(',');
 						putchar(' ');
