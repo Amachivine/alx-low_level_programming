ded972bda92cb1b4e82a55d40871c0bb7f995545
Amachivine <favour2apenefavour2apene@gmail.com>
2022-12-11 19:01:12 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints out 0 to 9 using puchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
